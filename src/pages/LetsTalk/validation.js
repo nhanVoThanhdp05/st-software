487fca3c146175c8381fc24ed49fc31ef40d1945
tunglqgithub <tung.le_dp05@devplus.edu.vn>
2023-03-20 16:06:59 +0700

    feat: dpm-reorganize-code

--- src/pages/LetsTalk/validation.js
+++ src/pages/LetsTalk/validation.js
@@ -11,8 +11,53 @@
             event.preventDefault();
             event.stopPropagation();
           }
+          
+
           form.classList.add('was-validated');
         }, false);
       });
     }, false);
   })();
+
+  // validation email
+//   const validateEmail = (email) => {
+//     return email.match(
+//       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+//   };
+  
+//   const validate = () => {
+//     const $result = $('#result');
+//     const email = $('#email').val();
+//     $result.text('');
+  
+//     if (validateEmail(email)) {
+//       $result.text(email + ' is valid :)');
+//       $result.css('color', 'green');
+//     } else {
+//       $result.text(email + ' is not valid :(');
+//       $result.css('color', 'red');
+//     }
+//     return false;
+//   }
+  
+//   $('#email').on('input', validate);
+  
+//   function validatePhoneNumber(input_str) {
+//     var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+//     return re.test(input_str);
+// }
+
+// function validateForm(event) {
+//     var phone = document.getElementById('myform_phone').value;
+//     if (!validatePhoneNumber(phone)) {
+//         document.getElementById('phone_error').classList.remove('hidden');
+//     } else {
+//         document.getElementById('phone_error').classList.add('hidden');
+//         alert("validation success")
+//     }
+//     event.preventDefault();
+// }
+
+// document.getElementById('myform').addEventListener('submit', validateForm);
